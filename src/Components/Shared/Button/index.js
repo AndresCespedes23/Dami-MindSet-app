ba6107888f88370649132431157a63c11726d09f
valeferaudo <valentinferaudo@gmail.com>
2021-11-25 17:26:48 -0300

    MS-80: Optimize code

--- src/Components/Shared/Button/index.js
+++ src/Components/Shared/Button/index.js
@@ -1,28 +1,12 @@
 import styles from './button.module.css';
 import { FaPlus, FaTrashAlt, FaEdit } from 'react-icons/fa';
-// eslint-disable-next-line react/prop-types
+
 function Button({ type }) {
   return (
-    <button
-      className={
-        type === 'add'
-          ? styles.buttonAdd
-          : type === 'delete'
-          ? styles.buttonDelete
-          : type === 'update'
-          ? styles.buttonUpdate
-          : styles.hide
-      }
-    >
-      {type === 'add' ? (
-        <FaPlus />
-      ) : type === 'delete' ? (
-        <FaTrashAlt />
-      ) : type === 'update' ? (
-        <FaEdit />
-      ) : (
-        <FaPlus />
-      )}
+    <button className={styles[type]}>
+      {type === 'add' && <FaPlus />}
+      {type === 'update' && <FaEdit />}
+      {type === 'delete' && <FaTrashAlt />}
     </button>
   );
 }
