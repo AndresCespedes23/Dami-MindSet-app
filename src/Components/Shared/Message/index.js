051eb9671a1f0ab24e803bcc6c0cbc9fe2cd29d4
Julian Vicente <julian.vicente97s@gmail.com>
2021-11-25 17:51:14 -0300

    MS-81: refactor styles selection

--- src/Components/Shared/Message/index.js
+++ src/Components/Shared/Message/index.js
@@ -1,19 +1,8 @@
 import styles from './message.module.css';
 
-// eslint-disable-next-line react/prop-types
 function Message({ type, message }) {
   return (
-    <div
-      className={
-        type === 'error'
-          ? styles.error
-          : type === 'success'
-          ? styles.success
-          : type === 'warning'
-          ? styles.warning
-          : styles.hide
-      }
-    >
+    <div className={styles[type] || styles.hide}>
       <p>{message}</p>
     </div>
   );
