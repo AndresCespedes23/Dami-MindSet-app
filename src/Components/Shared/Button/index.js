24750d540d5cc7e944a614ed9b373638bb4737da
valeferaudo <valentinferaudo@gmail.com>
2021-11-25 16:42:48 -0300

    MS-80: Update Button icons

--- src/Components/Shared/Button/index.js
+++ src/Components/Shared/Button/index.js
@@ -1,8 +1,8 @@
 import styles from './button.module.css';
 import PropTypes from 'prop-types';
-
+import { FaPlus, FaTrashAlt, FaEdit } from 'react-icons/fa';
 // eslint-disable-next-line react/prop-types
-function Button({ type, message }) {
+function Button({ type }) {
   return (
     <button
       className={
@@ -15,17 +15,23 @@ function Button({ type, message }) {
           : styles.hide
       }
     >
-      {message}
+      {type === 'add' ? (
+        <FaPlus />
+      ) : type === 'delete' ? (
+        <FaTrashAlt />
+      ) : type === 'update' ? (
+        <FaEdit />
+      ) : (
+        <FaPlus />
+      )}
     </button>
   );
 }
 
 Button.defaultProps = {
-  type: 'add',
-  message: 'Add'
+  type: 'add'
 };
 Button.prototype = {
-  type: PropTypes.string,
-  message: PropTypes.string
+  type: PropTypes.string
 };
 export default Button;
