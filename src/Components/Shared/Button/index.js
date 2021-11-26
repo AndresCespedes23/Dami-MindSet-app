b1cdd157a62c11d069a2b1dae30c8c9637d3666c
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-11-26 10:48:26 -0300

    MS-83: delete handleShowModal

--- src/Components/Shared/Button/index.js
+++ src/Components/Shared/Button/index.js
@@ -1,13 +1,13 @@
 import styles from './button.module.css';
 import { FaPlus, FaTrashAlt, FaEdit, FaRegWindowClose} from 'react-icons/fa';
 
-function Button({ type, handleShowModal }) {
+function Button({ type}) {
   return (
     <button className={styles[type]}>
       {type === 'add' && <FaPlus />}
       {type === 'update' && <FaEdit />}
       {type === 'delete' && <FaTrashAlt />}
-      {type === 'close' && <FaRegWindowClose onClick={handleShowModal} />}
+      {type === 'close' && <FaRegWindowClose />}
     </button>
   );
 }
