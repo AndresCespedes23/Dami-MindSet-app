8af5e1d01ad43de0427d570c9148609165fbbe00
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-11-26 01:40:07 -0300

    modal complete. form added previously deleted

---
+++ src/Components/Shared/Modal/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./modal.module.css";
+import Button from "../Button/index";
+
+function Modal() {
+  return (
+    <div className={styles.modalBackground}>
+      <div className={styles.modalContainer}>
+        <div className={styles.titleCloseBtn}>
+          <Button type="close" valor="close" handleShowModal={handleShowModal} />
+        </div>
+        <div className={styles.title}>
+          <h1>Example text</h1>
+        </div>
+        <div className={styles.body}>
+        </div>
+        <div className={styles.footer}>
+          <button >
+            Cancel
+          </button>
+          <button>Continue</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
