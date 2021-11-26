2906f618f23bffa735eef81d1b2281e03c0198be
Julian Vicente <julian.vicente97s@gmail.com>
2021-11-26 19:25:17 -0300

    MS-70: refactor Modal component

--- src/Components/Shared/Modal/index.js
+++ src/Components/Shared/Modal/index.js
@@ -2,21 +2,14 @@ import React from 'react';
 import styles from './modal.module.css';
 import Button from '../Button';
 
-function Modal({ handleShowModal }) {
+function Modal({ handleShowModal, component }) {
   return (
     <div className={styles.modalBackground}>
       <div className={styles.modalContainer}>
         <div className={styles.titleCloseBtn}>
           <Button type="close" onClick={handleShowModal} />
         </div>
-        <div className={styles.title}>
-          <h1>Example text</h1>
-        </div>
-        <div className={styles.body}></div>
-        <div className={styles.footer}>
-          <button className={styles.cancelBtn}>Cancel</button>
-          <button>Continue</button>
-        </div>
+        <div className={styles.body}>{component}</div>
       </div>
     </div>
   );
