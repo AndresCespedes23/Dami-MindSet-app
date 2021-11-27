5749ffb06c9b6833f41b7a772d939b75978f1009
Julian Vicente <julian.vicente97s@gmail.com>
2021-11-27 00:17:35 -0300

    MS-85: refactor modal logic

--- src/Components/Shared/Modal/index.js
+++ src/Components/Shared/Modal/index.js
@@ -2,20 +2,23 @@ import React from 'react';
 import styles from './modal.module.css';
 import Button from '../Button';
 import PostulantsForm from '../../Postulants/Form';
-import Confirmation from '../../Postulants/Confirmation';
 
-function Modal({ handleShowModal, modalType, meta, handleSubmit }) {
+function Modal({ handleShowModal, form, meta, handleSubmit }) {
+  let modalComponent;
+  switch (form) {
+    case 'clients':
+      modalComponent = <PostulantsForm id={meta.id} handleSubmit={handleSubmit} />;
+      break;
+  }
   return (
     <div className={styles.modalBackground}>
       <div className={styles.modalContainer}>
         <div className={styles.titleCloseBtn}>
           <Button type="close" onClick={handleShowModal} />
         </div>
-        {modalType === 'postulants' && <PostulantsForm handleSubmit={handleSubmit} meta={meta} />}
-        {modalType === 'deleteConfirmation' && <Confirmation />}
+        {modalComponent}
       </div>
     </div>
   );
 }
-
 export default Modal;
