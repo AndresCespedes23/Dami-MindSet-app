19ad77ce40860247d0d866a315a0cedabcfe54ae
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-11-26 23:13:11 -0300

    MS-78: Make a GET request to retrieve all the Admins

--- src/Components/Admins/index.js
+++ src/Components/Admins/index.js
@@ -1,6 +1,17 @@
+import { useState, useEffect } from 'react';
 import styles from './admins.module.css';
 
 function Admins() {
+  const [admins, setAdmins] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API}/admins`)
+      .then((response) => response.json())
+      .then((response) => {
+        setAdmins(response);
+      });
+  }, []);
+
   return (
     <section className={styles.container}>
       <h2>Admins</h2>
@@ -10,11 +21,20 @@ function Admins() {
             <th>Name</th>
             <th>Email</th>
             <th>Username</th>
-            <th>Password</th>
             <th>Action</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {admins.map((admin) => {
+            return (
+              <tr key={admin._id}>
+                <td>{admin.name}</td>
+                <td>{admin.email}</td>
+                <td>{admin.username}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </section>
   );
