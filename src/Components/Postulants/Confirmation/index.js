c2b3548a7d54c845317f24e9d556af398ce2856c
Julian Vicente <julian.vicente97s@gmail.com>
2021-11-28 16:28:21 -0300

    MS-70: add styles for Confirmation component

--- src/Components/Postulants/Confirmation/index.js
+++ src/Components/Postulants/Confirmation/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import Button from '../../Shared/Button';
+import styles from './confirmation.module.css';
 
 function Confirmation({ message, handleDelete, handleShowModal }) {
   return (
-    <div>
+    <div className={styles.container}>
       <p>{message}</p>
-      <Button
-        type="confirm"
-        onClick={() => {
-          handleDelete();
-          handleShowModal();
-        }}
-      >
-        Yes
-      </Button>
-      <Button type="cancel" onClick={handleShowModal}>
-        No
-      </Button>
+      <div className={styles.buttonsContainer}>
+        <Button
+          type="confirm"
+          onClick={() => {
+            handleDelete();
+            handleShowModal();
+          }}
+        >
+          Yes
+        </Button>
+        <Button type="cancel" onClick={handleShowModal}>
+          No
+        </Button>
+      </div>
     </div>
   );
 }
