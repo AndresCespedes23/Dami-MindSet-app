9bd09ab28a90b0d123f9590cc86b2a496368bfc1
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-11-27 23:19:21 -0300

    MS-78: addeForm

---
+++ src/Components/Admins/Form/index.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react';
+import styles from './form.module.css';
+
+function AdminsForm({ id, handleSubmit, handleShowModal }) {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    username: '',
+    password: ''
+  });
+
+  useEffect(() => {
+    if (id) {
+      fetch(`${process.env.REACT_APP_API}/admins/${id}`)
+        .then((response) => response.json())
+        .then((response) => {
+          setFormData(response);
+        });
+    }
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const newAdmin = {
+      name: event.target[0].value,
+      email: event.target[1].value,
+      username: event.target[2].value,
+      password: event.target[3].value
+    };
+    handleSubmit(newAdmin);
+    handleShowModal();
+  };
+
+  return (
+    <form className={styles.form} onSubmit={onSubmit}>
+      <div className={styles.formField}>
+        <label>Name</label>
+        <input type="text" name="name" value={formData.name} onChange={handleChange} />
+      </div>
+      <div className={styles.formField}>
+        <label>Email</label>
+        <input type="text" name="email" value={formData.email} onChange={handleChange} />
+      </div>
+      <div className={styles.formField}>
+        <label>Username</label>
+        <input type="text" name="username" value={formData.username} onChange={handleChange} />
+      </div>
+      <div className={styles.formField}>
+        <label>Password</label>
+        <input type="password" name="password" value={formData.password} onChange={handleChange} />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default AdminsForm;
