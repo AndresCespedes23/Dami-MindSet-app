9eb68b3f68eb6ec6924099e88a8be720d5986589
BareaMartin <martin.barea94@gmail.com>
2021-11-29 10:27:51 -0300

    MS-71: added case to modal for psychologists

--- src/Components/Shared/Modal/index.js
+++ src/Components/Shared/Modal/index.js
@@ -1,25 +1,49 @@
 import React from 'react';
 import styles from './modal.module.css';
 import Button from '../Button';
+import Confirmation from '../Confirmation';
+import PsychologistsForm from '../../Psychologists/Form';
 
-function Modal({ handleShowModal }) {
+function Modal({ handleShowModal, modalType, meta, handleSubmit }) {
+  let modalComponent;
+  switch (modalType) {
+    case 'postulants':
+      modalComponent = (
+        <PsychologistsForm
+          id={meta}
+          handleSubmit={handleSubmit}
+          handleShowModal={handleShowModal}
+        />
+      );
+      break;
+    case 'psychologists':
+      modalComponent = (
+        <PsychologistsForm
+          id={meta}
+          handleSubmit={handleSubmit}
+          handleShowModal={handleShowModal}
+        />
+      );
+      break;
+    case 'delete':
+      modalComponent = (
+        <Confirmation
+          message="Did you want to Delete?"
+          handleDelete={handleSubmit}
+          handleShowModal={handleShowModal}
+        />
+      );
+      break;
+  }
   return (
     <div className={styles.modalBackground}>
       <div className={styles.modalContainer}>
         <div className={styles.titleCloseBtn}>
           <Button type="close" onClick={handleShowModal} />
         </div>
-        <div className={styles.title}>
-          <h1>Example text</h1>
-        </div>
-        <div className={styles.body}></div>
-        <div className={styles.footer}>
-          <button className={styles.cancelBtn}>Cancel</button>
-          <button>Continue</button>
-        </div>
+        {modalComponent}
       </div>
     </div>
   );
 }
-
 export default Modal;
