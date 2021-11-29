1f3bb6f18424b6f0219b951de1c654eb41dbfa32
Paloma Quiroz <palomaxquiroz@gmail.com>
2021-11-29 15:21:03 -0300

    MS-73: confirmation shared component added

---
+++ src/Components/Shared/Confirmation/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './confirmation.module.css';
+import Button from '../Button';
+
+function Confirmation({ message, handleDelete, handleShowModal }) {
+  return (
+    <div>
+      <p>{message}</p>
+      <div className={styles.container}>
+        <Button
+          type="confirm"
+          onClick={() => {
+            handleDelete();
+            handleShowModal();
+          }}
+        />
+        <Button type="cancel" onClick={handleShowModal} />
+      </div>
+    </div>
+  );
+}
+
+export default Confirmation;
