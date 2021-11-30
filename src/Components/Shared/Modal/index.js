cd17e5979ba3ea7e37acd4db2b10c4e07ee1c400
Paloma Quiroz <palomaxquiroz@gmail.com>
2021-11-30 14:12:10 -0300

    MS-73: Added import

--- src/Components/Shared/Modal/index.js
+++ src/Components/Shared/Modal/index.js
@@ -2,11 +2,43 @@ import React from 'react';
 import styles from './modal.module.css';
 import Button from '../Button';
 import SessionsForm from '../../Sessions/Form';
+import AdminsForm from '../../Admins/Form';
 import Confirmation from '../Confirmation';
+import PsychologistsForm from '../../Psychologists/Form';
+import PositionsForm from '../../Positions/Form';
+import InterviewForm from '../../Interviews/Form';
+import PostulantsForm from '../../Postulants/Form';
+import ProfilesForm from '../../Profiles/Form';
+import ApplicationsForm from '../../Applications/Form';
 
 function Modal({ handleShowModal, modalType, meta, handleSubmit }) {
   let modalComponent;
   switch (modalType) {
+    case 'admins':
+      modalComponent = (
+        <AdminsForm id={meta} handleSubmit={handleSubmit} handleShowModal={handleShowModal} />
+      );
+      break;
+    case 'positions':
+      modalComponent = (
+        <PositionsForm id={meta} handleSubmit={handleSubmit} handleShowModal={handleShowModal} />
+      );
+      break;
+    case 'interviews':
+      modalComponent = (
+        <InterviewForm id={meta} handleSubmit={handleSubmit} handleShowModal={handleShowModal} />
+      );
+      break;
+    case 'postulants':
+      modalComponent = (
+        <PostulantsForm id={meta} handleSubmit={handleSubmit} handleShowModal={handleShowModal} />
+      );
+      break;
+    case 'profiles':
+      modalComponent = (
+        <ProfilesForm id={meta} handleSubmit={handleSubmit} handleShowModal={handleShowModal} />
+      );
+      break;
     case 'sessions':
       modalComponent = (
         <SessionsForm id={meta} handleSubmit={handleSubmit} handleShowModal={handleShowModal} />
@@ -19,6 +51,20 @@ function Modal({ handleShowModal, modalType, meta, handleSubmit }) {
           handleDelete={handleSubmit}
           handleShowModal={handleShowModal}
         />
+      );
+      break;
+    case 'psychologists':
+      modalComponent = (
+        <PsychologistsForm
+          id={meta}
+          handleSubmit={handleSubmit}
+          handleShowModal={handleShowModal}
+        />
+      );
+      break;
+    case 'applications':
+      modalComponent = (
+        <ApplicationsForm id={meta} handleSubmit={handleSubmit} handleShowModal={handleShowModal} />
       );
       break;
   }
