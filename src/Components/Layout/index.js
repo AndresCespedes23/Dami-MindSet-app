fcde2f951a4edab8b0d839d9f61aabfdb2eedb55
Julian Vicente <julian.vicente97s@gmail.com>
2021-12-01 19:51:09 -0300

    MS-104: add Link elements for better navigation

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from '../Shared/Header/index';
 import Footer from '../Shared/Footer/index';
 import Admins from '../Admins/index';
@@ -17,23 +17,21 @@ function Layout() {
   return (
     <div className={styles.container}>
       <Header />
-      <Router>
-        <Switch>
-          <Route path="/admins" component={Admins} />
-          <Route path="/applications" component={Applications} />
-          <Route path="/clients" component={Clients} />
-          <Route path="/interviews" component={Interviews} />
-          <Route path="/positions" component={Positions} />
-          <Route path="/postulants" component={Postulants} />
-          <Route path="/profiles" component={Profiles} />
-          <Route path="/psychologists" component={Psychologists} />
-          <Route path="/sessions" component={Sessions} />
-          <Route path="/home" component={Home} />
-          <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/admins" component={Admins} />
+        <Route path="/applications" component={Applications} />
+        <Route path="/clients" component={Clients} />
+        <Route path="/interviews" component={Interviews} />
+        <Route path="/positions" component={Positions} />
+        <Route path="/postulants" component={Postulants} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/psychologists" component={Psychologists} />
+        <Route path="/sessions" component={Sessions} />
+        <Route path="/home" component={Home} />
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
