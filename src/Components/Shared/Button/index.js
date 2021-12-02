0c95bdcc15c0aef30172c81081bd0acb76db3bf6
valeferaudo <valentinferaudo@gmail.com>
2021-12-02 00:57:59 -0300

    MS-111: Add shared Button component (Add ... & submit & Search ...)

--- src/Components/Shared/Button/index.js
+++ src/Components/Shared/Button/index.js
@@ -1,13 +1,16 @@
 import styles from './button.module.css';
 import { FaPlus, FaTrashAlt, FaEdit, FaRegWindowClose } from 'react-icons/fa';
 
-function Button({ type, onClick }) {
+function Button({ type, onClick, text }) {
   return (
     <button className={styles[type]} onClick={onClick}>
       {type === 'add' && <FaPlus />}
       {type === 'update' && <FaEdit />}
       {type === 'delete' && <FaTrashAlt />}
       {type === 'close' && <FaRegWindowClose />}
+      {type === 'submit' && 'Submit'}
+      {type === 'search' && `Search ${text ? text : ''}`}
+      {type === 'addNew' && `Add new ${text ? text : ''}`}
       {type === 'confirm' && 'YES'}
       {type === 'cancel' && 'NO'}
     </button>
