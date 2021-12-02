cf8a4bfe4d50e0cbbb5b5509c16a465a111203e7
Sofía Pagura <sofiapagura@gmail.com>
2021-12-02 20:13:44 -0300

    MS122: add reset css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from './Components/Layout';
 import './index.css';
+import './reset.css';
 
 ReactDOM.render(
   <React.StrictMode>
