ecb5b3d30ab32e2f9c33543b852139d0c7f8058a
BareaMartin <martin.barea94@gmail.com>
2021-12-09 15:17:15 -0300

    MS-133: added update logic

--- src/redux/Applications/thunks.js
+++ src/redux/Applications/thunks.js
@@ -7,8 +7,13 @@ import {
   ADD_APPLICATIONS_REJECTED,
   DELETE_APPLICATIONS_FETCHING,
   DELETE_APPLICATIONS_FULFILLED,
-  DELETE_APPLICATIONS_REJECTED
+  DELETE_APPLICATIONS_REJECTED,
+  UPDATE_APPLICATIONS_FETCHING,
+  UPDATE_APPLICATIONS_FULFILLED,
+  UPDATE_APPLICATIONS_REJECTED
 } from '../../constants/actionTypes';
+
+const URL = `${process.env.REACT_APP_API}/applications`;
 
 const getApplicationsFetching = () => ({
   type: GET_APPLICATIONS_FETCHING
@@ -22,8 +27,6 @@ const getApplicationsFulfilled = (payload) => ({
 const getApplicationsRejected = () => ({
   type: GET_APPLICATIONS_REJECTED
 });
-
-const URL = `${process.env.REACT_APP_API}/applications`;
 
 export const getApplications = () => {
   return (dispatch) => {
@@ -68,11 +71,11 @@ const addApplicationRejected = () => ({
 
 export const addApplication = (application) => (dispatch) => {
   dispatch(addApplicationFetching());
-  fetch(URL, {
+  return fetch(URL, {
     method: 'POST',
     mode: 'cors',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json; charset=UTF-8'
     },
     body: JSON.stringify(application)
   })
@@ -168,5 +171,60 @@ export const deleteApplication = (id) => (dispatch) => {
 //       setShowMessage(true);
 //       setMessageType('error');
 //       setMessage('Error deleting application');
+//     });
+// };
+const updateApplicationFetching = () => ({
+  type: UPDATE_APPLICATIONS_FETCHING
+});
+
+const updateApplicationFullfiled = (payload, id) => ({
+  type: UPDATE_APPLICATIONS_FULFILLED,
+  payload,
+  id
+});
+
+const updateApplicationRejected = () => ({
+  type: UPDATE_APPLICATIONS_REJECTED
+});
+
+export const updateApplication = (application, id) => (dispatch) => {
+  dispatch(updateApplicationFetching());
+  return fetch(`${URL}/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(application)
+  })
+    .then((data) => data.json())
+    .then(() => {
+      dispatch(updateApplicationFullfiled(id));
+    })
+    .catch(() => {
+      dispatch(updateApplicationRejected());
+    });
+};
+
+// const handleUpdateApplication = (application) => {
+//   fetch(`${process.env.REACT_APP_API}/applications/${idActive}`, {
+//     method: 'PUT',
+//     headers: {
+//       'Content-type': 'application/json; charset=UTF-8'
+//     },
+//     body: JSON.stringify(application)
+//   })
+//     .then((response) => {
+//       if (response.status === 200 || response.status === 201) return response.json();
+//       throw new Error(`HTTP ${response.status}`);
+//     })
+//     .then(() => {
+//       setShowMessage(true);
+//       setMessageType('success');
+//       setMessage('Application updated');
+//       getApplications();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       setShowMessage(true);
+//       setMessageType('error');
+//       setMessage('Error updating application');
 //     });
 // };
