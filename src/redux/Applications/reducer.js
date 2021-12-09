4f298c998d7813c1c1cf8f73ffa86a9b847546d3
BareaMartin <martin.barea94@gmail.com>
2021-12-09 12:16:10 -0300

    MS-133: added delete logic

--- src/redux/Applications/reducer.js
+++ src/redux/Applications/reducer.js
@@ -4,10 +4,10 @@ import {
   GET_APPLICATIONS_REJECTED,
   ADD_APPLICATIONS_FETCHING,
   ADD_APPLICATIONS_FULFILLED,
-  ADD_APPLICATIONS_REJECTED
-  //   DELETE_APPLICATIONS_FETCHING,
-  //   DELETE_APPLICATIONS_FULFILLED,
-  //   DELETE_APPLICATIONS_REJECTED
+  ADD_APPLICATIONS_REJECTED,
+  DELETE_APPLICATIONS_FETCHING,
+  DELETE_APPLICATIONS_FULFILLED,
+  DELETE_APPLICATIONS_REJECTED
 } from '../../constants/actionTypes';
 const initialState = {
   isLoading: false,
@@ -55,23 +55,23 @@ const applicationsReducer = (state = initialState, action) => {
         isLoading: false,
         error: true
       };
-    // case DELETE_APPLICATIONS_FETCHING:
-    //   return {
-    //     ...state,
-    //     isLoading: true
-    //   };
-    // case DELETE_APPLICATIONS_FULFILLED:
-    //   return {
-    //     ...state,
-    //     isLoading: false,
-    //     list: state.list.filter((application) => application._id !== action.payload)
-    //   };
-    // case DELETE_APPLICATIONS_REJECTED:
-    //   return {
-    //     ...state,
-    //     isLoading: false,
-    //     error: true
-    //   };
+    case DELETE_APPLICATIONS_FETCHING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case DELETE_APPLICATIONS_FULFILLED:
+      return {
+        ...state,
+        isLoading: false,
+        list: state.list.filter((application) => application._id !== action.payload)
+      };
+    case DELETE_APPLICATIONS_REJECTED:
+      return {
+        ...state,
+        isLoading: false,
+        error: true
+      };
     default:
       return state;
   }
