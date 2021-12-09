b9272fcb912099eb2e7a9e9642e4a676960faf7a
BareaMartin <martin.barea94@gmail.com>
2021-12-09 11:24:17 -0300

    MS-133: combined applications to rootReducers

--- src/redux/reducers/rootReducers.js
+++ src/redux/reducers/rootReducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import adminsReducer from '../Admins/reducer';
+import applicationsReducer from '../Applications/reducer';
 
 const rootReducer = combineReducers({
-  admins: adminsReducer
+  admins: adminsReducer,
+  applications: applicationsReducer
 });
 
 export default rootReducer;
