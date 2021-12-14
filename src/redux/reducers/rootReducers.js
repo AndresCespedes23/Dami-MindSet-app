084b7daeb9817f4bfedc0293f66387f093fba9ca
Paloma Quiroz <palomaxquiroz@gmail.com>
2021-12-13 21:28:24 -0300

    MS-138: Added root reducers

--- src/redux/reducers/rootReducers.js
+++ src/redux/reducers/rootReducers.js
@@ -3,12 +3,14 @@ import adminsReducer from '../Admins/reducer';
 import applicationsReducer from '../Applications/reducer';
 import sessionsReducer from '../Sessions/reducer';
 import positionsReducer from '../Positions/reducer';
+import profilesReducer from '../Profiles/reducer';
 
 const rootReducer = combineReducers({
   admins: adminsReducer,
   applications: applicationsReducer,
   sessions: sessionsReducer,
-  positions: positionsReducer
+  positions: positionsReducer,
+  profiles: profilesReducer
 });
 
 export default rootReducer;
