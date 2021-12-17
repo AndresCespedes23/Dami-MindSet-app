9c449fdafcaa638836bf9d0f401b8054a7a8bccc
valeferaudo <valentinferaudo@gmail.com>
2021-12-17 17:05:57 -0300

    MS-163: Initial html and styles. Without funtionality

---
+++ src/Components/Postulant/Register/index.js
@@ -0,0 +1,111 @@
+import { useSelector } from 'react-redux';
+import { Form, Field } from 'react-final-form';
+import Input from 'Components/Shared/Input';
+import styles from './register.module.css';
+import Button from 'Components/Shared/Button';
+
+function Register() {
+  const error = useSelector((store) => store.postulants.error);
+
+  const onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+
+  const validate = (formValues) => {
+    const errors = {};
+    if (!formValues.username) {
+      errors.username = 'Username is required';
+    }
+    if (formValues.name?.length < 3) {
+      errors.name = 'Name must be at least 3 characters';
+    }
+    return errors;
+  };
+
+  const required = (value) => (value ? undefined : 'Required');
+  return (
+    <section className={styles.container}>
+      <div className={styles.containerBlock}>
+        <div className={styles.containerForm}>
+          <Form
+            onSubmit={onSubmit}
+            validate={validate}
+            render={(formProps) => (
+              <form className={styles.containerInput} onSubmit={formProps.handleSubmit}>
+                <div className={styles.itemInputColumn}>
+                  <h3 className={styles.titleInput}>Name</h3>
+                  <Field
+                    component={Input}
+                    name="name"
+                    typeInput="text"
+                    errorMessage="Name is missing"
+                    error={error.name}
+                    disabled={formProps.submitting}
+                    validate={required}
+                  />
+                </div>
+                <div className={styles.itemInputColumn}>
+                  <h3 className={styles.titleInput}>Surname</h3>
+                  <Field
+                    component={Input}
+                    name="username"
+                    typeInput="text"
+                    errorMessage="Username is missing"
+                    error={error.username}
+                    disabled={formProps.submitting}
+                    validate={required}
+                  />{' '}
+                </div>
+                <div className={styles.itemInputColumnLong}>
+                  <h3 className={styles.titleInput}>Email</h3>
+                  <Field
+                    component={Input}
+                    name="email"
+                    typeInput="text"
+                    errorMessage="Email is missing"
+                    error={error.email}
+                    disabled={formProps.submitting}
+                    validate={required}
+                  />
+                </div>
+                <div className={styles.itemInputColumn}>
+                  <h3 className={styles.titleInput}>Password</h3>
+                  <Field
+                    component={Input}
+                    name="password"
+                    typeInput="password"
+                    errorMessage="Password is missing"
+                    error={error.password}
+                    disabled={formProps.submitting}
+                    validate={required}
+                  />
+                </div>
+                <div className={styles.itemInputColumn}>
+                  <h3 className={styles.titleInput}>Repeat password</h3>
+                  <Field
+                    component={Input}
+                    name="repeatPassword"
+                    typeInput="text"
+                    errorMessage="Repeat password is missing or invalid"
+                    error={error.repeatPassword}
+                    disabled={formProps.submitting}
+                    validate={required}
+                  />
+                </div>
+                <div className={styles.containerFooter}>
+                  <button className={styles.btnRegister}>REGISTER</button>
+                  <span className={styles.messageSign}>
+                    Already have an account? <a>Sign In!</a>
+                  </span>
+                  <Button type="submit" />
+                </div>
+              </form>
+            )}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Register;
--- src/Routes/postulants.js
+++ src/Routes/postulants.js
@@ -2,12 +2,14 @@ import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import Home from 'Components/Postulant/Home';
 import Layout from 'Components/Layout';
 import Sign from 'Components/Postulant/Sign';
+import Register from 'Components/Postulant/Register';
 
 const PostulantRoutes = () => {
   const { url } = useRouteMatch();
   return (
     <Layout routes={[{ name: 'Go to admin app', path: '/admin' }]} styleType="postulant">
       <Switch>
+        <Route path={`${url}/register`} component={Register} />
         <Route path={`${url}/sign`} component={Sign} />
         <Route path={`${url}/`} exact component={Home} />
       </Switch>
