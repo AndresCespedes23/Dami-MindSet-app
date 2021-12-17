f6a4fee21706cdd8e8338fe8ad21b240a423bc0b
Sofía Pagura <sofiapagura@gmail.com>
2021-12-16 23:35:29 -0300

    MS-171: woorking on style

--- src/Components/Postulant/Profile/index.js
+++ src/Components/Postulant/Profile/index.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getOnePostulant } from 'redux/Postulants/thunks';
 import Button from 'Components/Shared/Button';
-// import styles from './profile.modile.css';
+import style from './profile.module.css';
 
 function Profile() {
   const postulant = useSelector((state) => state.postulants.postulant);
@@ -20,59 +20,61 @@ function Profile() {
   };
 
   return (
-    <section>
-      <div>
-        <Button type={'back'} />
-        <h2>Profile</h2>
-      </div>
-      <div>
+    <section className={style.container}>
+      <div className={style.profile}>
         <div>
-          <p>About me</p>
-          <Button type={'editInfo'} />
-        </div>
-        <div>DESCRIPCIÓN de dónde sacaba esto manolo</div>
-      </div>
-      <div>
-        <div>
-          <h3>PERSONAL INFORMATION</h3>
-          <Button type={'editInfo'} />
+          <Button type={'back'} />
+          <h2>Profile</h2>
         </div>
         <div>
           <div>
-            <p>Name:</p>
-            <span>{postulant.name}</span>
+            <p>About me</p>
+            <Button type={'editInfo'} />
+          </div>
+          <div>DESCRIPCIÓN de dónde sacaba esto manolo</div>
+        </div>
+        <div>
+          <div>
+            <h3>PERSONAL INFORMATION</h3>
+            <Button type={'editInfo'} />
           </div>
           <div>
-            <p>Surname:</p>
-            <span>{postulant.surname}</span>
-          </div>
-          <div>
-            <p>Email</p>
-            <span>{postulant.email}</span>
-          </div>
-          <div>
-            <p>Date of birth:</p>
-            <span>{postulant.dateOfBirth ? postulant.dateOfBirth.split('T')[0] : ''}</span>
-          </div>
-          <div>
-            <p>Age:</p>
-            <span>{postulant.dateOfBirth ? getAge(postulant.dateOfBirth) : ''}</span>
-          </div>
-          <div>
-            <p>Phone number:</p>
-            <span>{postulant.phoneNumber}</span>
-          </div>
-          <div>
-            <p>Address:</p>
-            <span>{postulant.address}</span>
-          </div>
-          <div>
-            <p>City:</p>
-            <span>{postulant.city}</span>
-          </div>
-          <div>
-            <p>Postal Code:</p>
-            <span>{postulant.zipCode}</span>
+            <div>
+              <p>Name:</p>
+              <span>{postulant.name}</span>
+            </div>
+            <div>
+              <p>Surname:</p>
+              <span>{postulant.surname}</span>
+            </div>
+            <div>
+              <p>Email</p>
+              <span>{postulant.email}</span>
+            </div>
+            <div>
+              <p>Date of birth:</p>
+              <span>{postulant.dateOfBirth ? postulant.dateOfBirth.split('T')[0] : ''}</span>
+            </div>
+            <div>
+              <p>Age:</p>
+              <span>{postulant.dateOfBirth ? getAge(postulant.dateOfBirth) : ''}</span>
+            </div>
+            <div>
+              <p>Phone number:</p>
+              <span>{postulant.phoneNumber}</span>
+            </div>
+            <div>
+              <p>Address:</p>
+              <span>{postulant.address}</span>
+            </div>
+            <div>
+              <p>City:</p>
+              <span>{postulant.city}</span>
+            </div>
+            <div>
+              <p>Postal Code:</p>
+              <span>{postulant.zipCode}</span>
+            </div>
           </div>
         </div>
       </div>
