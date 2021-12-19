08a50ae32078e18623760dfb9b6f29a040197b64
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-12-19 16:37:51 -0300

    MS-166: add the route for work path

--- src/Routes/postulants.js
+++ src/Routes/postulants.js
@@ -1,5 +1,6 @@
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import Home from 'Components/Postulant/Home';
+import Work from 'Components/Postulant/Work';
 import Layout from 'Components/Layout';
 import Sign from 'Components/Postulant/Sign';
 
@@ -10,6 +11,7 @@ const PostulantRoutes = () => {
       <Switch>
         <Route path={`${url}/sign`} component={Sign} />
         <Route path={`${url}/`} exact component={Home} />
+        <Route path={`${url}/work`} exact component={Work} />
       </Switch>
     </Layout>
   );
