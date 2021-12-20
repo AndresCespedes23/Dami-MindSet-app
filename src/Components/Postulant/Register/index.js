deeb5b43eabc30b971735319c69ce77a210c65d2
valeferaudo <valentinferaudo@gmail.com>
2021-12-20 09:53:57 -0300

    MS-163: Update form styles

--- src/Components/Postulant/Register/index.js
+++ src/Components/Postulant/Register/index.js
@@ -21,89 +21,92 @@ function Register() {
   const required = (value) => (value ? undefined : 'Required');
   return (
     <section className={styles.container}>
-      <div className={styles.containerBlock}>
-        <div className={styles.containerForm}>
-          <Form
-            onSubmit={onSubmit}
-            validate={validate}
-            render={(formProps) => (
-              <form className={styles.containerInput} onSubmit={formProps.handleSubmit}>
-                <div className={styles.itemInputColumn}>
-                  <h3 className={styles.titleInput}>Name</h3>
-                  <Field
-                    component={Input}
-                    name="name"
-                    typeInput="text"
-                    placeholder="Enter your name"
-                    errorMessage="Name is missing"
-                    error={error.name}
-                    disabled={formProps.submitting}
-                    validate={required}
-                  />
+      <div className={styles.containerSummary}>
+        <h2 className={styles.title}>Personal Information</h2>
+        <Form
+          onSubmit={onSubmit}
+          validate={validate}
+          render={(formProps) => (
+            <form onSubmit={formProps.handleSubmit}>
+              <div className={styles.formContent}>
+                <div className={styles.inputColumn}>
+                  <div>
+                    <Field
+                      component={Input}
+                      label="Name"
+                      name="name"
+                      typeInput="text"
+                      placeholder="Enter your name"
+                      errorMessage="Name is missing"
+                      error={error.name}
+                      disabled={formProps.submitting}
+                      validate={required}
+                    />
+                  </div>
+                  <div>
+                    <Field
+                      component={Input}
+                      label="Username"
+                      name="username"
+                      typeInput="text"
+                      errorMessage="Username is missing"
+                      placeholder="Enter your username"
+                      error={error.username}
+                      disabled={formProps.submitting}
+                      validate={required}
+                    />
+                  </div>
                 </div>
-                <div className={styles.itemInputColumn}>
-                  <h3 className={styles.titleInput}>Surname</h3>
-                  <Field
-                    component={Input}
-                    name="username"
-                    typeInput="text"
-                    errorMessage="Username is missing"
-                    placeholder="Enter your username"
-                    error={error.username}
-                    disabled={formProps.submitting}
-                    validate={required}
-                  />{' '}
+                <Field
+                  component={Input}
+                  label="Email"
+                  name="email"
+                  typeInput="text"
+                  errorMessage="Email is missing"
+                  placeholder="Enter your email"
+                  error={error.email}
+                  disabled={formProps.submitting}
+                  validate={required}
+                />
+                <div className={styles.inputColumn}>
+                  <div>
+                    <Field
+                      component={Input}
+                      label="Password"
+                      name="password"
+                      typeInput="password"
+                      type="password"
+                      errorMessage="Password is missing"
+                      placeholder="Enter your password"
+                      error={error.password}
+                      disabled={formProps.submitting}
+                      validate={required}
+                    />
+                  </div>
+                  <div>
+                    <Field
+                      component={Input}
+                      name="repeatPassword"
+                      label="Repeat password"
+                      type="password"
+                      errorMessage="Repeat password is missing or invalid"
+                      placeholder="Enter your password"
+                      error={error.repeatPassword}
+                      disabled={formProps.submitting}
+                      validate={required}
+                    />
+                  </div>
                 </div>
-                <div className={styles.itemInputColumnLong}>
-                  <h3 className={styles.titleInput}>Email</h3>
-                  <Field
-                    component={Input}
-                    name="email"
-                    typeInput="text"
-                    errorMessage="Email is missing"
-                    placeholder="Enter your email"
-                    error={error.email}
-                    disabled={formProps.submitting}
-                    validate={required}
-                  />
-                </div>
-                <div className={styles.itemInputColumn}>
-                  <h3 className={styles.titleInput}>Password</h3>
-                  <Field
-                    component={Input}
-                    name="password"
-                    typeInput="password"
-                    type="password"
-                    errorMessage="Password is missing"
-                    placeholder="Enter your password"
-                    error={error.password}
-                    disabled={formProps.submitting}
-                    validate={required}
-                  />
-                </div>
-                <div className={styles.itemInputColumn}>
-                  <h3 className={styles.titleInput}>Repeat password</h3>
-                  <Field
-                    component={Input}
-                    name="repeatPassword"
-                    type="password"
-                    errorMessage="Repeat password is missing or invalid"
-                    placeholder="Enter your password"
-                    error={error.repeatPassword}
-                    disabled={formProps.submitting}
-                    validate={required}
-                  />
-                </div>
-                <div className={styles.containerFooter}>
-                  <button className={styles.btnRegister}>REGISTER</button>
-                  <span className={styles.messageSign}>
-                    Already have an account? <a>Sign In!</a>
-                  </span>
-                </div>
-              </form>
-            )}
-          />
-        </div>
+              </div>
+              <div className={styles.containerFooter}>
+                <button className={styles.btnRegister}>REGISTER</button>
+                <span className={styles.messageSign}>
+                  Already have an account? <a>Sign In!</a>
+                </span>
+              </div>
+            </form>
+          )}
+        />
       </div>
     </section>
   );
