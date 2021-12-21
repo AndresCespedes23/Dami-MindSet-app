3684dd602eb52a39869275a146d5c955da9e0219
Julian Vicente <julian.vicente97s@gmail.com>
2021-12-21 12:20:49 -0300

    MS-185: set constant for actionsTypes

--- src/redux/PostulantModule/actions.js
+++ src/redux/PostulantModule/actions.js
@@ -1,4 +1,11 @@
-export const setRegisterInfo = (payload) => ({ type: 'SET_REGISTER_INFO', payload });
-export const setPersonalInfo = (payload) => ({ type: 'SET_PERSONAL_INFO', payload });
-export const setEducationInfo = (payload) => ({ type: 'SET_EDUCATION_INFO', payload });
-export const setExperienceInfo = (payload) => ({ type: 'SET_WORK_EXPERIENCE_INFO', payload });
+import {
+  SET_REGISTER_INFO,
+  SET_PERSONAL_INFO,
+  SET_EDUCATION_INFO,
+  SET_WORK_EXPERIENCE_INFO
+} from 'constants/actionTypes';
+
+export const setRegisterInfo = (payload) => ({ type: SET_REGISTER_INFO, payload });
+export const setPersonalInfo = (payload) => ({ type: SET_PERSONAL_INFO, payload });
+export const setEducationInfo = (payload) => ({ type: SET_EDUCATION_INFO, payload });
+export const setExperienceInfo = (payload) => ({ type: SET_WORK_EXPERIENCE_INFO, payload });
--- src/redux/PostulantModule/reducer.js
+++ src/redux/PostulantModule/reducer.js
@@ -1,3 +1,10 @@
+import {
+  SET_REGISTER_INFO,
+  SET_PERSONAL_INFO,
+  SET_EDUCATION_INFO,
+  SET_WORK_EXPERIENCE_INFO
+} from 'constants/actionTypes';
+
 const initialState = {
   registerInfo: {},
   personalInfo: {},
@@ -7,22 +14,22 @@ const initialState = {
 
 const postulantsModuleReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'SET_REGISTER_INFO':
+    case SET_REGISTER_INFO:
       return {
         ...state,
         registerInfo: action.payload
       };
-    case 'SET_PERSONAL_INFO':
+    case SET_PERSONAL_INFO:
       return {
         ...state,
         personalInfo: action.payload
       };
-    case 'SET_EDUCATION_INFO':
+    case SET_EDUCATION_INFO:
       return {
         ...state,
         educationInfo: [...state.educationInfo, action.payload]
       };
-    case 'SET_WORK_EXPERIENCE_INFO':
+    case SET_WORK_EXPERIENCE_INFO:
       return {
         ...state,
         worlExperienceInfo: [...state.worlExperienceInfo, action.payload]
