b2480c3b1a747c33eabeb2b5b6118b6e96a677f1
Julian Vicente <julian.vicente97s@gmail.com>
2021-12-26 20:17:26 -0300

    MS-189: remove sign up button

--- src/Routes/auth.js
+++ src/Routes/auth.js
@@ -2,10 +2,7 @@ import { Switch, Route, Redirect, useRouteMatch } from 'react-router-dom';
 import Login from 'Components/Auth/Login';
 import Layout from 'Components/Layout';
 
-const authRoutes = [
-  { name: 'Login', path: '/auth/login' },
-  { name: 'Sign up', path: '/auth/sign-up' }
-];
+const authRoutes = [{ name: 'Login', path: '/auth/login' }];
 
 const AuthRoutes = () => {
   const { url } = useRouteMatch();
