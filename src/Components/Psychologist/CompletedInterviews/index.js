9b03c5ed28ca502818bde7e1aadbcd3593b0a0f2
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-12-27 17:13:22 -0300

    MS-196: basic flexbox & style

--- src/Components/Psychologist/CompletedInterviews/index.js
+++ src/Components/Psychologist/CompletedInterviews/index.js
@@ -1,20 +1,60 @@
 import styles from './completed-interviews.module.css';
 import Button from 'Components/Shared/Button';
 
-function Description() {
+function CompletedInterviews() {
   return (
     <section className={styles.container}>
       <div className={styles.containerInterviews}>
         <Button type={'back'} />
-        <h3 className={styles.subtitle}>
-          <span className={styles.bold}>Unassigned users:</span>
-        </h3>
-        <h3 className={styles.subtitle}>
-          <span className={styles.bold}>Completed Interviews:</span>
-        </h3>
+        <div className={styles.interviewsContent}>
+          <h3 className={styles.title}>
+            <span className={styles.bold}>Unassigned users:</span>
+          </h3>
+          <div className={styles.interviewsInfo}>
+            <div className={styles.user}>
+              <p className={styles.userName}>Ruben</p>
+              <p>Viernes 24 de Diciembre</p>
+            </div>
+            <div>
+              <button className={styles.assignBtn}>ASSING PROFILE</button>
+            </div>
+          </div>
+          <div className={styles.interviewsInfo}>
+            <div className={styles.user}>
+              <p className={styles.userName}>Jose</p>
+              <p>Sabado 25 de Diciembre</p>
+            </div>
+            <div>
+              <button className={styles.assignBtn}>ASSING PROFILE</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h3 className={styles.title}>
+            <span className={styles.bold}>Completed Interviews:</span>
+          </h3>
+          <div className={styles.interviewsInfo}>
+            <div className={styles.user}>
+              <p className={styles.userName}>Matias</p>
+              <p className={styles.date}>Viernes 17 de Agosto</p>
+            </div>
+            <div>
+              <button className={styles.detailsBtn}>DETAILS</button>
+            </div>
+          </div>
+          <div className={styles.interviewsInfo}>
+            <div className={styles.user}>
+              <p className={styles.userName}>Manuel</p>
+              <p className={styles.date}>Sabado 9 de Julio</p>
+            </div>
+            <div>
+              <button className={styles.detailsBtn}>DETAILS</button>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
 }
 
-export default Description;
+export default CompletedInterviews;
