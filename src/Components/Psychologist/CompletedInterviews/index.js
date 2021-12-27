aabf79a500eec26aecd7d1bb61352209acfbed2a
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-12-27 18:23:54 -0300

    MS-196: implement fetch to put users inside the div

--- src/Components/Psychologist/CompletedInterviews/index.js
+++ src/Components/Psychologist/CompletedInterviews/index.js
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getInterviews } from 'redux/Interviews/thunks';
 import styles from './completed-interviews.module.css';
 import Button from 'Components/Shared/Button';
+import { FaCheckCircle, FaClock } from 'react-icons/fa';
 
 function CompletedInterviews() {
+  const interviews = useSelector((state) => state.interviews.list);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getInterviews());
+  }, [dispatch]);
   return (
     <section className={styles.container}>
       <div className={styles.containerInterviews}>
@@ -10,47 +20,51 @@ function CompletedInterviews() {
           <h3 className={styles.title}>
             <span className={styles.bold}>Unassigned users:</span>
           </h3>
-          <div className={styles.interviewsInfo}>
-            <div className={styles.user}>
-              <p className={styles.userName}>Ruben</p>
-              <p>Viernes 24 de Diciembre</p>
-            </div>
-            <div>
-              <button className={styles.assignBtn}>ASSING PROFILE</button>
-            </div>
-          </div>
-          <div className={styles.interviewsInfo}>
-            <div className={styles.user}>
-              <p className={styles.userName}>Jose</p>
-              <p>Sabado 25 de Diciembre</p>
-            </div>
-            <div>
-              <button className={styles.assignBtn}>ASSING PROFILE</button>
-            </div>
-          </div>
+          <table>
+            <tbody>
+              {interviews.map((interview) => {
+                if (interview.idCandidate && interview.idClient && interview.idPosition) {
+                  return [
+                    <tr key={interview._id} className={styles.interviewsInfo}>
+                      <td className={styles.userName}>{interview.idCandidate.name}</td>
+                      <td>{interview.dateTime.split('T')[0]}</td>
+                      <td className={styles[interview.status.toLowerCase()]}>
+                        {interview.status === 'DONE' ? <FaCheckCircle /> : <FaClock />}
+                      </td>
+                      <td>
+                        <button className={styles.assignBtn}>ASSING PROFILE</button>
+                      </td>
+                    </tr>
+                  ];
+                }
+              })}
+            </tbody>
+          </table>
         </div>
         <div>
           <h3 className={styles.title}>
             <span className={styles.bold}>Completed Interviews:</span>
           </h3>
-          <div className={styles.interviewsInfo}>
-            <div className={styles.user}>
-              <p className={styles.userName}>Matias</p>
-              <p className={styles.date}>Viernes 17 de Agosto</p>
-            </div>
-            <div>
-              <button className={styles.detailsBtn}>DETAILS</button>
-            </div>
-          </div>
-          <div className={styles.interviewsInfo}>
-            <div className={styles.user}>
-              <p className={styles.userName}>Manuel</p>
-              <p className={styles.date}>Sabado 9 de Julio</p>
-            </div>
-            <div>
-              <button className={styles.detailsBtn}>DETAILS</button>
-            </div>
-          </div>
+          <table>
+            <tbody>
+              {interviews.map((interview) => {
+                if (interview.idCandidate && interview.idClient && interview.idPosition) {
+                  return [
+                    <tr key={interview._id} className={styles.interviewsInfo}>
+                      <td className={styles.userName}>{interview.idCandidate.name}</td>
+                      <td>{interview.dateTime.split('T')[0]}</td>
+                      <td className={styles[interview.status.toLowerCase()]}>
+                        {interview.status === 'DONE' ? <FaCheckCircle /> : <FaClock />}
+                      </td>
+                      <td>
+                        <button className={styles.detailsBtn}>DETAILS</button>
+                      </td>
+                    </tr>
+                  ];
+                }
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
     </section>
