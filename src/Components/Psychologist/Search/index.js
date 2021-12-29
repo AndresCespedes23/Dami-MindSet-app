0d21d07e37054ab3e410cb163172500902965cb8
valeferaudo <valentinferaudo@gmail.com>
2021-12-28 21:04:33 -0300

    MS-199: update reducer, actions and thunks to getPostulants by name

--- src/Components/Psychologist/Search/index.js
+++ src/Components/Psychologist/Search/index.js
@@ -1,11 +1,27 @@
-// import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { searchPostulants } from 'redux/Postulants/thunks';
 import styles from './search.module.css';
 
 function Search() {
-  //   const interviews = useSelector((state) => state.interviews.list);
-  //   const isLoadingForm = useSelector((state) => state.interviews.isLoadingForm);
-  //   const dispatch = useDispatch();
+  const postulants = useSelector((state) => state.postulants.list);
+  const isLoading = useSelector((state) => state.postulants.isLoading);
+  const dispatch = useDispatch();
+  const [inputSearch, setInputSearch] = useState('');
 
+  const handleChange = (e) => {
+    setInputSearch(e.target.value);
+  };
+  const handleEnter = (e) => {
+    if (e.code === 'Enter') {
+      dispatch(searchPostulants(inputSearch));
+    }
+  };
+  const handleSubmit = () => {
+    dispatch(searchPostulants(inputSearch));
+  };
+  console.log(postulants);
+  console.log(isLoading);
   return (
     <section className={styles.container}>
       <div className={styles.containerInterviews}>
@@ -15,15 +31,23 @@ function Search() {
           </div>
           <div className={styles.searchContainer}>
             <div className={styles.itemPersonalColumn}>
-              <h2 className={styles.searchTitle}>Search user</h2>
-              <input className={styles.searchInput} placeholder="User" />
+              <h2 className={styles.searchTitle}>Search Postulant</h2>
+              <input
+                className={styles.searchInput}
+                placeholder="Postulant"
+                value={inputSearch}
+                onChange={handleChange}
+                onKeyPress={handleEnter}
+              />
             </div>
             <div>
-              <button className={styles.searchBtn}>SEARCH</button>
+              <button type="submit" className={styles.searchBtn} onClick={handleSubmit}>
+                SEARCH
+              </button>
             </div>
           </div>
         </div>
-        <div className={styles.interviewsContent}></div>
+        <div className={styles.interviewsContent}>{postulants.length && <div>Hola</div>}</div>
       </div>
     </section>
   );
