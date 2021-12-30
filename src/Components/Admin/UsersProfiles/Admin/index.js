aab9a384a9c965b1b76ff9b7cb05484565b40c3f
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-12-30 20:05:38 -0300

    MS-218: organize the info with flexbox & added styles

--- src/Components/Admin/UsersProfiles/Admin/index.js
+++ src/Components/Admin/UsersProfiles/Admin/index.js
@@ -1,7 +1,45 @@
+import styles from './admin.module.css';
+import Button from 'Components/Shared/Button';
+
 function AdminProfile() {
   return (
-    <section>
-      <h2>I am Admin</h2>
+    <section className={styles.container}>
+      <div className={styles.profile}>
+        <div className={styles.header}>
+          <div>
+            <Button type={'back'} />
+          </div>
+          <div className={styles.headercolumn}>
+            <h2 className={styles.profileTitle}>Profile</h2>
+            <span className={styles.adminTitle}>Administrator</span>
+          </div>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.rowInfo}>
+            <div className={styles.adminInfo}>
+              Name:<span>roberto</span>
+            </div>
+            <div className={styles.adminInfo}>
+              Username:<span>sandro</span>
+            </div>
+          </div>
+          <div className={styles.columnInfo}>
+            <div className={styles.adminInfo}>
+              Email:<span>sandroRosa@gmail.com</span>
+            </div>
+            <div className={styles.adminInfo}>
+              Phone Number:<span>12314325424</span>
+            </div>
+          </div>
+          <div className={styles.editBtn}>
+            <Button type={'editInfo'} />
+          </div>
+        </div>
+        <div className={styles.footer}>
+          <span>Status</span>
+          <p className={styles.statusActive}>ACTIVE</p>
+        </div>
+      </div>
     </section>
   );
 }
