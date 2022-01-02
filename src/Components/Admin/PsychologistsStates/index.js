7db124460de7586745befdf486422b47ce6765ac
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-01-02 13:17:27 -0300

    MS-215: added basic style

--- src/Components/Admin/PsychologistsStates/index.js
+++ src/Components/Admin/PsychologistsStates/index.js
@@ -1,8 +1,27 @@
+import styles from './psychologists-states.module.css';
+import Button from 'Components/Shared/Button';
+
 function PsychologistsStates() {
   return (
-    <div>
-      <h1>I am psycho state</h1>
-    </div>
+    <section className={styles.container}>
+      <div className={styles.containerPsychologists}>
+        <div className={styles.header}>
+          <Button type={'backBtnAdmin'} />
+          <h2>Psychologists</h2>
+        </div>
+        <div className={styles.contentPsychologists}>
+          <h3 className={styles.title}>
+            <span className={styles.bold}>Pending Approval</span>
+          </h3>
+          <h3 className={styles.title}>
+            <span className={styles.bold}>Active Psychologists</span>
+          </h3>
+          <h3 className={styles.title}>
+            <span className={styles.bold}>Disabled Psychologists</span>
+          </h3>
+        </div>
+      </div>
+    </section>
   );
 }
 
