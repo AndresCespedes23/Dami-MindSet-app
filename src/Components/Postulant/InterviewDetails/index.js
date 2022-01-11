517aef9f3bbda7d52842a72bdfa74d72ee1ef971
Sofía Pagura <sofiapagura@gmail.com>
2022-01-11 00:02:04 -0300

    MS-232: bring data

--- src/Components/Postulant/InterviewDetails/index.js
+++ src/Components/Postulant/InterviewDetails/index.js
@@ -1,20 +1,17 @@
-import Spinner from 'Components/Shared/Spinner';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import { getPendingInterview } from 'redux/Interviews/thunks';
-import { getOnePostulant } from 'redux/Postulants/thunks';
+import { getPendingInterview } from 'redux/Interviews/thunks';
 import style from './interviewDetails.module.css';
-// import { useHistory } from 'react-router-dom';
-
 import Button from 'Components/Shared/Button';
+import Spinner from 'Components/Shared/Spinner';
 
 function InterviewDetails() {
-  const postulant = useSelector((store) => store.postulants.postulant);
+  const interviews = useSelector((store) => store.interviews.list);
   const isLoading = useSelector((store) => store.postulants.isLoading);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getOnePostulant(sessionStorage.getItem('id')));
+    dispatch(getPendingInterview(sessionStorage.getItem('id')));
   }, [dispatch]);
 
   if (isLoading) return <Spinner type="ThreeDots" color="#002147" height={80} width={80} />;
@@ -26,43 +23,43 @@ function InterviewDetails() {
           <Button type={'back'} />
           <h2>Interview Details</h2>
         </div>
-        <div className={style.box}>
-          <div className={style.info}>
-            <h3>Status:</h3>
-            <span
-              className={
-                postulant.status === 'ACTIVE'
-                  ? style.statusActive
-                  : postulant.status === 'INACTIVE'
-                  ? style.statusInactive
-                  : style.statusPending
-              }
-            >
-              {postulant.status}
-            </span>
-          </div>
-          <div className={style.info}>
-            <h3>Position:</h3>
-            <span>{postulant.username}</span>
-          </div>
-          <div className={style.info}>
-            <h3>Company:</h3>
-            <span>{postulant.username}</span>
-          </div>
-          <div className={style.info}>
-            <h3>Location:</h3>
-            <span>{postulant.username}</span>
-          </div>
-        </div>
-        <div className={style.box}>
-          <h3>About:</h3>
-          <span>{postulant.username} </span>
-        </div>
-        <div className={style.box}>
-          <div className={style.info}>
-            <h3>Date:</h3>
-            <span>{postulant.username}</span>
-          </div>
+        <div>
+          {interviews.map((interview) => {
+            return (
+              <div key={interview._id}>
+                <div className={style.box}>
+                  <div className={style.info}>
+                    <h3>Status:</h3>
+                    <span>{interview.status}</span>
+                  </div>
+                  <div className={style.info}>
+                    <h3>Position:</h3>
+                    <span>{interview.idPosition.name}</span>
+                  </div>
+                  <div className={style.info}>
+                    <h3>Company:</h3>
+                    <span>{interview.idClient.name}</span>
+                  </div>
+                  <div className={style.info}>
+                    <h3>Location:</h3>
+                    <span>{interview.idPosition.address},</span>
+                    <span>{interview.idPosition.city},</span>
+                    <span>{interview.idPosition.postalCode},</span>
+                  </div>
+                </div>
+                <div className={style.box}>
+                  <h3>About:</h3>
+                  <span>{interview.idPosition.description}</span>
+                </div>
+                <div className={style.box}>
+                  <div className={style.info}>
+                    <h3>Date:</h3>
+                    <span>{interview.dateTime.split('T')[0]}</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
